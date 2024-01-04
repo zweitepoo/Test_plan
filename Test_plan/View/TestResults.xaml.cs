93220ecc4ea8faefa437d8518aaf07b78659bbde
KMadej1 <kuba.madej@rockwellautomation.com>
2024-01-04 19:06:56 +0100

    Basic file explorer added

---
+++ Test_plan/View/TestResults.xaml.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Test_plan
+{
+    /// <summary>
+    /// Interaction logic for TestResults.xaml
+    /// </summary>
+    public partial class TestResults : Page
+    {
+        #region Constructor
+        ResultsDisplay castResults;
+        MainWindow MainWindow;
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="mainWindow"></param>
+        public TestResults(MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            InitializeComponent();          
+            castResults = this.Resources["results"] as ResultsDisplay;
+            string curDir = Directory.GetCurrentDirectory(); 
+            
+           // this.WeBrowserResults.Navigate(@"file://C:\8.Csharp\testhtml.html");
+            
+
+        }
+        #endregion
+
+        /// <summary>
+        /// When the application fisrt opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        #region On loaded
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Get every logical drive
+          foreach(var drive in Directory.GetLogicalDrives())
+            {
+                // Create a new item for it
+                var item = new TreeViewItem()
+                {
+                    // Set the header and path
+                    Header = drive.ToString(),
+                    Tag = drive,
+                };
+
+               
+
+                item.Items.Add(null);
+
+                //Listen out for items being expanded
+                item.Expanded += Folder_Expanded;
+
+                // Add it to the main tree view
+                FolderView.Items.Add(item);
+            }
+        }
+        #endregion
+
+        #region Folder expanded
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            #region Initial checks
+            var item  = (TreeViewItem)sender;
+
+            //if the item only contains the dummy data
+
+            if (item.Items.Count != 1 && item.Items[0] != null)
+                return;
+            item.Items.Clear();
+            //Get full path
+            var fullPath= (string)item.Tag;
+            #endregion
+
+            #region Get directories
+
+            var directories  = new List<string>();
+            try
+            {
+                var dirs  = Directory.GetDirectories(fullPath);
+                    
+                if (dirs.Length>0)
+                    directories.AddRange(dirs);               
+            }
+            catch { }
+
+
+            directories.ForEach(directoryPath =>
+            {
+                //Create directory item
+                var subItem = new TreeViewItem() 
+                {
+                    Header= GetFileFolderName(directoryPath),
+                    //tag as full path
+                    Tag = directoryPath,
+
+                };
+
+                //Add dummy item so we can expand folder
+                subItem.Items.Add(null);
+                subItem.Expanded += Folder_Expanded;
+
+                //Add to parent
+                item.Items.Add(subItem);    
+            });
+            #endregion
+
+            #region Get files
+            var files = new List<string>();
+            try
+            {
+                var fileDirs = Directory.GetFiles(fullPath);
+
+                if (fileDirs.Length > 0)
+                    files.AddRange(fileDirs);
+            }
+            catch { }
+
+
+            files.ForEach(filesPath =>
+            {
+                //Create file item
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(filesPath),
+                    //tag as full path
+                    Tag = filesPath,
+
+                };                             
+
+                //Add to parent
+                item.Items.Add(subItem);
+                #endregion
+            });
+
+            }
+
+        #endregion
+        /// <summary>
+        /// File the file or folder name from a full path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static string GetFileFolderName(string path)
+        {
+            // C:\Something\a folder
+            // C:\Something\a file.png
+
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            // make all slashes backslashes
+            var normalizedPath = path.Replace('/', '\\');
+            // find the last backslash
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            if (lastIndex <= 0)
+                return path;
+
+            return path.Substring(lastIndex + 1);
+
+            
+        }
+
+        private void NavigateTestPlan_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.NavigateTestPlanPage();
+        }
+
+    }
+}
---
+++ Test_plan/ViewModel/HeaderToImageConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Test_plan
+{
+    /// <summary>
+    /// Convert full path to an image of a file, folder, drive
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        
+        
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {//Get full path
+
+            var path = (string)value;
+            if (path == null) 
+                return null;
+            var name = TestResults.GetFileFolderName(path);
+            var imagePath = @"Graphics/Drive_64.png";
+
+            if (string.IsNullOrEmpty(name))
+                imagePath = @"Graphics/Drive_64.png";
+            else if( new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory) )
+                imagePath = @"Graphics/Folder_64.png";
+            else 
+                imagePath = @"Graphics/File_64.png";
+            var image = new BitmapImage(new Uri($"pack://application:,,,/{imagePath}"));
+            return image;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
